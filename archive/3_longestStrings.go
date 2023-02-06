340a2fcf80ebe80db4f421e2565ebc165c661bdf
jeyrce <jeyrce@gmail.com>
2023-02-06 09:24:14 +0800

    找到不重复最长子串

--- archive/3_longestStrings.go
+++ archive/3_longestStrings.go
@@ -21,6 +21,28 @@ package archive
 // 
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+
+// 思路: 从第一个开始，用map保存字符串，如果出现重复则map清零，并移动开始位置到下一个直到所有检查完
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	var (
+		max int
+	)
+	for i := 0; i <= len(s)-1; i++ {
+		k := string(s[i])
+		m := make(map[string]struct{}, len(s)-i-1)
+		m[k] = struct{}{}
+		// 检查是否有重复字符
+		for j := i + 1; j <= len(s)-1; j++ {
+			next := string(s[j])
+			if _, ok := m[next]; !ok {
+				m[next] = struct{}{}
+				continue
+			}
+		}
+		// 一轮走完检查字符串长度
+		if cur := len(m); max < cur {
+			max = cur
+		}
+	}
+	return max
 }
